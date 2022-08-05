74016b7ec4fcd0a173abdf0de5716eab413efbb1
Brian <briansuarezsantiago@gmail.com>
2022-08-05 15:05:03 +0100

    Kata 1. Version 2

--- src/agecalculator/Main.java
+++ src/agecalculator/Main.java
@@ -1,21 +1,21 @@
 package agecalculator;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 /**
  * Class that initializes an object of type Person
  * in order to represent a person and obtain the
  * person's age.
  *
  * @author Brian Suárez Santiago
- * @version 1.0.0
- * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Date.html">Class Date</a>
- * @deprecated As of JDK version 1.1 Date class is deprecated and replaced by Calendar class.
+ * @version 2.0.0
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Calendar.html">Class Calendar</a>
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/GregorianCalendar.html">Class GregorianCalendar</a>
  */
 public class Main {
 
-    @Deprecated
     public static void main(String[] args) {
-        Person person = new Person("Brian", new Date(101, 7, 28));
+        Person person = new Person("Brian", new GregorianCalendar(2001, Calendar.AUGUST, 28));
         System.out.println(person.getName() + " has " + person.getAge() + " years old.");
     }
 }
--- src/agecalculator/Person.java
+++ src/agecalculator/Person.java
@@ -1,22 +1,20 @@
 package agecalculator;
 
-import java.util.Date;
+import java.util.Calendar;
 /**
  * Class representing a person. The class has the necessary
  * public methods to obtain all the information needed to
  * work with a person instance. The class makes use of the
- * deprecated Java Date library.
- *
- * The class Date represents a specific instant in time,
- * with millisecond precision.
+ * Java Calendar and GregorianCalendar utilities.
  *
  * @author Brian Suárez Santiago
- * @version 1.0.0
- * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Date.html">Class Date</a>
+ * @version 2.0.0
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Calendar.html">Class Calendar</a>
  */
 public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     /**
      * Constructor initializing the attributes of a person.
@@ -24,7 +22,7 @@ public class Person {
      * @param name Name of the person.
      * @param birthdate Date of birth of the person.
      */
-    public Person(String name, Date birthdate) {
+    public Person(String name, Calendar birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -43,19 +41,30 @@ public class Person {
      *
      * @return Date of birth of the person.
      */
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return this.birthdate;
     }
 
     /**
      * Returns the age of a person through the
-     * difference in milliseconds between the
-     * current date and the person's date of
-     * birth.
+     * difference in milliseconds between the current date
+     * and the person's date of birth.
      *
      * @return Age of the person.
      */
     public int getAge() {
-        return (int) ((new Date().getTime() - this.birthdate.getTime())/31536000000L);
+        Calendar currentDate = Calendar.getInstance();
+        return (int) (milliSecondsToYear(currentDate.getTimeInMillis() - this.birthdate.getTimeInMillis()));
+    }
+
+    /**
+     * Converts milliseconds passed as a
+     * parameter into years.
+     *
+     * @param millis Milliseconds to convert to years.
+     * @return Years equivalent to the milliseconds passed as an argument.
+     */
+    private long milliSecondsToYear(long millis) {
+        return millis/MILLISECONDS_PER_YEAR;
     }
 }
